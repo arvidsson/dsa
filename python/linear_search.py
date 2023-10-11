a7b8adf42d9e34c558ef96c34f227947d669af45
arvidsson <arvidsson@gmail.com>
2023-10-11 12:40:53 +0200

    added unit test

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,9 +1,6 @@
+import sys
+import unittest
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("item", type=int, help="The item to search for")
-parser.add_argument("array", nargs="+", type=int, help="The array to search in")
-args = parser.parse_args()
 
 
 def linear_search(array, item):
@@ -14,8 +11,26 @@ def linear_search(array, item):
     return False
 
 
-# eg python3 linear_search.py 2 1 2 3 5 8
-if linear_search(args.array, args.item):
-    print("Found!")
-else:
-    print("Not found...")
+class Test(unittest.TestCase):
+    def test_found(self):
+        self.assertTrue(linear_search([1, 2, 3, 4, 5], 2))
+
+    def test_not_found(self):
+        self.assertFalse(linear_search([1, 2, 3, 4, 5], 10))
+
+    def test_empty_list(self):
+        self.assertFalse(linear_search([], 10))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        unittest.main()
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("item", type=int, help="The item to search for")
+        parser.add_argument("array", nargs="+", type=int, help="The array to search in")
+        args = parser.parse_args()
+        if linear_search(args.array, args.item):
+            print("Found!")
+        else:
+            print("Not found...")
